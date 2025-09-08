9eaed84dc147bc0607bbf486ef8aa8bf2af7c4bd
Mya Von Behren <mvonbehr@depaul.edu>
2025-09-07 21:00:05 -0400

    Refactor Footer component to improve layout styling and enhance visual consistency with updated border properties.

--- app/shared/layout/Footer.tsx
+++ app/shared/layout/Footer.tsx
@@ -2,11 +2,11 @@ import { ThemeSwitcher } from "../theme/theme-switcher";
 
 export default function Footer() {
   return (
-    <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-    <p>
-      Powered by UniCal
+    <footer className="w-full flex items-center justify-center border-t border-sidebar-border mx-auto text-center text-xs py-2">
+      <p>
+        Powered by UniCal
       </p>
-    <ThemeSwitcher />
-  </footer>
+      <ThemeSwitcher />
+    </footer>
   );
 }
