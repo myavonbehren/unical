883d305cbb34394e9def806e06d8d0c1e64483a6
Mya Von Behren <mvonbehr@depaul.edu>
2025-09-07 22:21:09 -0400

    Add NoSemesters component to display a message when no semesters are available, enhancing user feedback in the SemestersPage.

---
+++ app/dashboard/semesters/(ui)/no-semesters.tsx
@@ -0,0 +1,10 @@
+export default function NoSemesters() {
+  return (
+    <div className="flex flex-col items-center justify-center h-full">
+      <h1 className="font-semibold font-heading text-xl">No Semesters</h1>
+      <p className="text-sm text-muted-foreground">
+        You don't have any semesters yet.
+      </p>
+    </div>
+  );
+}
