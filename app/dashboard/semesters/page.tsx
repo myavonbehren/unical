791033f7b77d1d92031b4fcb4b6c72212f4b54ba
Mya Von Behren <mvonbehr@depaul.edu>
2025-09-07 22:21:01 -0400

    Enhance SemestersPage layout by adding a header with an "Add Semester" button and integrating NoSemesters component for improved user experience.

--- app/dashboard/semesters/page.tsx
+++ app/dashboard/semesters/page.tsx
@@ -1,3 +1,18 @@
+import { Button } from "@/app/shared/ui/button";
+import NoSemesters from "./(ui)/no-semesters";
+
 export default function SemestersPage() {
-    return <div>Semesters</div>;
-  }
+    return <div className="h-full flex flex-col">
+      {/* Header */}
+      <div className="flex justify-between items-center mb-2">
+        <h1 className="font-semibold font-heading text-xl">Semesters</h1>
+        <Button>Add Semester</Button>
+      </div>
+
+      {/* Content */}
+      <div className="flex-1">
+        <NoSemesters />
+      </div>
+
+    </div>
+}
