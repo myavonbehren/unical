45efe70be221fb6cb757b11e90aa9806bd167327
Mya Von Behren <mvonbehr@depaul.edu>
2025-09-12 21:24:36 -0400

    Add dashboard layout index file to export all dashboard-related components, ensuring a centralized structure for easier imports and improved organization.

---
+++ app/shared/layout/dashboard/index.ts
@@ -0,0 +1,12 @@
+// Dashboard Layout Components
+export { default as DashboardPage } from './DashboardPage'
+export { default as DashboardPageHeader } from './DashboardPageHeader'
+export { default as DashboardPageContent } from './DashboardPageContent'
+export { default as DashboardPageTitle } from './DashboardPageTitle'
+
+// Existing components
+export { default as SideNav } from './SideNav'
+export { default as MobileSideNav } from './MobileSideNav'
+export { default as NavLinks } from './NavLinks'
+export { default as MobileNavLinks } from './MobileNavLinks'
+export { default as SidebarContext, SidebarProvider, useSidebar } from './SidebarContext'
